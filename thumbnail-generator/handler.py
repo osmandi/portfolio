6d06fea65f5b1f97f48f6f2a8fd9ed613822dfbf
osmandi <osmandigt@gmail.com>
2024-01-24 09:02:55 -0500

    Implement configuration to deploy

--- thumbnail-generator/handler.py
+++ thumbnail-generator/handler.py
@@ -6,38 +6,39 @@ Author: http://github.com/osmandi
 
 import boto3
 from PIL import Image
+from os import environ
 thumbnail_size = (128, 128)
 
 def thumbnail_generator(filename_original: str, filename_thumbnail: str):
 
     with Image.open(filename_original) as im:
         im.thumbnail(thumbnail_size)
-        im.save(f"/tmp/{filename_thumbnail}")
+        im.save(f"{filename_thumbnail}")
 
 def main(event, context):
 
-    s3_bucket = "local-bucket"
+    s3_bucket = "sls-etl"
     s3_key = event["Records"][0]["s3"]["object"]["key"]
     filename = s3_key.split("/")[-1].split(".")[0]
     extension = s3_key.split("/")[-1].split(".")[1]
-    filename_original = f"{filename}.{extension}"
-    filename_thumbnail = f"{filename}_thumbnail.{extension}"
+    filename_thumbnail_base = f"{filename}_thumbnail.{extension}"
+    filename_original = f"/tmp/{filename}.{extension}"
+    filename_thumbnail = f"/tmp/{filename_thumbnail_base}"
  
     # Start s3 client
-    s3 = boto3.client("s3",
-                      endpoint_url = "http://localhost:4569", # serverless-s3-local plugin
-                      aws_access_key_id = "S3RVER", # In prod use environment variable
-                      aws_secret_access_key = "S3RVER" # In prod use environment variable
+    s3 = boto3.client("s3", 
+       aws_access_key_id = environ["SLS_AWS_ACCESS_KEY"],
+       aws_secret_access_key = environ["SLS_AWS_SECRET_KEY"]
     )
 
     # Download image
-    s3.download_file(s3_bucket, s3_key, f'./{filename}.{extension}')
+    s3.download_file(s3_bucket, s3_key, filename_original)
 
     # Generate thumbnail
     thumbnail_generator(filename_original, filename_thumbnail)
 
     # Put file to S3
-    s3.upload_file(f"/tmp/{filename_thumbnail}", s3_bucket, f"output/{filename_thumbnail}")
+    s3.upload_file(f"{filename_thumbnail}", s3_bucket, f"output/{filename_thumbnail_base}")
 
     return {
         "message": "Thumbnail generated",
