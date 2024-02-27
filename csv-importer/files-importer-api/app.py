d88609f1f4c664a21a591111a15adcd24f1da146
osmandi <osmandigt@gmail.com>
2024-02-27 10:37:02 -0500

    Implement save csv to AWS S3

--- csv-importer/files-importer-api/app.py
+++ csv-importer/files-importer-api/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
+import boto3 # Inside AWS Lambda Runtime
 
 app = Flask(__name__)
 
@@ -12,7 +13,20 @@ def hello_from_root():
 def hello():
     return jsonify(message='Hello from path!')
 
-
 @app.errorhandler(404)
 def resource_not_found(e):
     return make_response(jsonify(error='Not found!'), 404)
+
+@app.route("/upload", methods=['POST'])
+def upload_files():
+    if 'csv' in request.files:
+        # If file is a csv
+        print("There are a file")
+        file = request.files.get('csv')
+        # Export to S3
+        bucket_name = "osmandi-sls-csv-importer"
+        s3 = boto3.resource("s3")
+        s3.Bucket(bucket_name).upload_fileobj(file, "test.csv")
+    else:
+        return jsonify(message="Not csv file"), 400
+    return jsonify(message="Hello")
