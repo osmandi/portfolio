2054a270685ca6f3c3c8e93f536be560723c4862
osmandi <osmandigt@gmail.com>
2024-02-28 05:55:09 -0500

    Adding hired endpoint to get number of hired

--- csv-importer/files-importer-api/app.py
+++ csv-importer/files-importer-api/app.py
@@ -12,6 +12,37 @@ def hello_from_root():
 def resource_not_found(e):
     return make_response(jsonify(error='Not found!'), 404)
 
+@app.route("/hired", methods=["GET"])
+def hired():
+    from time import sleep
+    # Get environment variables
+    aws_access_key_id=environ["awsAccessKey"]
+    aws_secret_access_key=environ["awsSecretKey"]
+    region=environ["region"]
+    redshift_identifier=environ["redshiftIdentifier"]
+    redshift_db_name=environ["redshiftDbName"]
+    redshift_username=environ["redshiftUsername"]
+    
+    client_redshift = boto3.client('redshift-data', region_name=region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    query_hired = """
+    SELECT d.id id, d.department, count(1) AS hired
+    FROM hired_employees he
+    INNER JOIN departments d ON d.id = he.department_id
+    WHERE he.datetime between '2021-01-01' and '2021-12-31'
+    GROUP BY d.id, d.department;
+    """
+    response = client_redshift.execute_statement(ClusterIdentifier=redshift_identifier, Database=redshift_db_name, DbUser=redshift_username, Sql=query_hired)
+
+    # Wait query execute_statement
+    query_status = client_redshift.describe_statement(Id=response["Id"])
+    while query_status["Status"] not in ["FINISHED", "ABORTED", "FAILED"]:
+        query_status = client_redshift.describe_statement(Id=response["Id"])
+        print(query_status)
+        sleep(1) # Wait for 1 second
+
+    result = client_redshift.get_statement_result(Id=response["Id"])
+    return make_response(result, 200)
+
 @app.route("/employees", methods=["GET"])
 def employees():
     from time import sleep
